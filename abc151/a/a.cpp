86870aa6912bc21fa8810953d07d0a048c516596
zo0005 <aaaaaright123@gmail.com>
2020-01-14 01:30:36 +0900

    add ABC 151 a

---
+++ abc151/a/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    char c;
+    cin >> c;
+
+    string a = "abcdefghijklmnopqrstuvwxyz";
+
+    cout << a[(c - 'a' + 1)] << endl;
+    return 0;
+}
