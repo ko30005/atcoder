87de9e10b9ef80b664f6ae50a2502208887d89e7
zo0005 <aaaaaright123@gmail.com>
2020-01-15 20:04:57 +0900

    add ABC150 a

---
+++ abc150/a/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k, x;
+    cin >> k >> x;
+
+    if(k * 500 >= x) {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    cout << "No" << endl;
+    return 0;
+}
